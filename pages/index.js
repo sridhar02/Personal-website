bf3445a61191b2c4dc6477cea7b8edc750ce5d1c
Sridhar Katta <kattasridhar02@gmail.com>
2020-04-18 13:24:21 +0530

    contact page is perfect and others layout has be set.

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,15 @@ import Grid from "@material-ui/core/Grid";
 import { Typography, makeStyles } from "@material-ui/core";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
+import MailIcon from "@material-ui/icons/Mail";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
 // import project from "../images/"
 
 const useIntroStyles = makeStyles({
   maincontent: {
+    minHeight: "100vh",
     marginTop: "50px",
     display: "flex",
     flexDirection: "column",
@@ -17,7 +22,25 @@ const useIntroStyles = makeStyles({
 });
 
 function Navabar() {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <a href="/">Home </a>
+      </div>
+      <div>
+        <a href="#skills">Skills</a>
+      </div>
+      <div>
+        <a href="#Projects">Projects</a>
+      </div>
+      <div>
+        <a href="#Contact">Contact</a>
+      </div>
+      <div>
+        <a href="#Experience">Experience</a>
+      </div>
+    </div>
+  );
 }
 
 function Intro() {
@@ -40,7 +63,7 @@ const useAboutStyles = makeStyles({});
 function AboutMe() {
   const classes = useAboutStyles();
   return (
-    <div>
+    <div id="AboutMe">
       <Typography variant="h5">What I hope to do</Typography>
       <Typography>
         My mission as a developer is to contribute to applications that empower
@@ -52,12 +75,36 @@ function AboutMe() {
   );
 }
 
+const useEXperienceStyles = makeStyles({
+  root: {
+    minHeight: "100vh",
+  },
+});
+
 function Experience() {
-  return <div></div>;
+  const classes = useEXperienceStyles();
+  return (
+    <div id="Experience" className={classes.root}>
+      <h4>Work Experience</h4>
+      <p>
+        I have worked for a number of projects. But, below are the corporate
+        experiences that I have.
+      </p>
+      <div>
+        <p>Insert company logo </p>
+        <p>Cambridge Technologies</p>
+        <div>
+          <h4>Reacjs Developer</h4>
+          <p>Jan 2020 to April 20202</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 const useSkillsStyles = makeStyles({
   root: {
+    minHeight: "100vh",
     minWidth: "275",
   },
 });
@@ -85,6 +132,9 @@ function Skills() {
 }
 
 const useProjectStyles = makeStyles({
+  main: {
+    minHeight: "100vh",
+  },
   root: {
     display: "flex",
     // flexDirection: "column",
@@ -100,7 +150,7 @@ const useProjectStyles = makeStyles({
 function Projects() {
   const classes = useProjectStyles();
   return (
-    <React.Fragment>
+    <div id="Projects" className={classes.main}>
       <h4>Projects</h4>
       <div className={classes.root} id="projects">
         <div className={classes.card}>
@@ -158,7 +208,87 @@ function Projects() {
           </p>
         </div>
       </div>
-    </React.Fragment>
+    </div>
+  );
+}
+
+const useContactStyles = makeStyles({
+  root: {
+    minHeight: "100vh",
+    margin: "auto 0px",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: "3.332rem",
+  },
+  icons: {
+    fontSize: "75px",
+    marginLeft: "100px",
+    marginRight: "100px",
+    marginTop: "50px",
+    marginBottom: "50px",
+  },
+  anchor: {
+    textDecoration: "None",
+    color: "black",
+  },
+  iconsLayout: {
+    display: "flex",
+    justifyContent: "space-around",
+  },
+});
+
+function Contact() {
+  const classes = useContactStyles();
+  return (
+    <div id="Contact" className={classes.root}>
+      <h4>Feel free to reach me out at:</h4>
+      <br />
+      <div className={classes.iconsLayout}>
+        <div>
+          <a
+            className={classes.anchor}
+            href="mailto:kattasridhar02@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <MailIcon className={classes.icons} />
+          </a>
+        </div>
+        <a
+          className={classes.anchor}
+          href="https://github.com/sridhar02"
+          target="_blank"
+        >
+          <GitHubIcon className={classes.icons} />
+        </a>
+        <a
+          className={classes.anchor}
+          href="https://www.linkedin.com/in/sridhar02"
+          target="_blank"
+        >
+          <LinkedInIcon className={classes.icons} />
+        </a>
+        <a
+          className={classes.anchor}
+          href="https://twitter.com/sridhar_K_1997"
+          target="_blank"
+        >
+          <TwitterIcon className={classes.icons} />
+        </a>
+      </div>
+      <br />
+      <p>
+        You can view my resume by clicking{" "}
+        <a
+          href="https://drive.google.com/file/d/1HNFiw7AdiypVvrfRk-DTa_yvxv3sxQbN/view?usp=sharing"
+          target="_blank"
+        >
+          here.
+        </a>
+      </p>
+    </div>
   );
 }
 
@@ -167,7 +297,17 @@ function App() {
     <Grid container>
       <Grid container>
         <Grid item md={12}>
+          <Navabar />
+        </Grid>
+      </Grid>
+      <Grid container>
+        <Grid item md={12}>
           <Intro />
+        </Grid>
+      </Grid>
+      <Grid container>
+        <Grid item md={12}>
+          <Experience />
         </Grid>
       </Grid>
       <Grid container>
@@ -178,6 +318,11 @@ function App() {
       <Grid container>
         <Grid item sm={12} md={12}>
           <Projects />
+        </Grid>
+      </Grid>
+      <Grid container>
+        <Grid item sm={12} md={12}>
+          <Contact />
         </Grid>
       </Grid>
     </Grid>
